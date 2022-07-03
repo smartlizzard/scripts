c4fe7993d11225cb1a49d2dda14a40aa2c167789
smartlizzard <32848463+smartlizzard@users.noreply.github.com>
2022-07-03 15:38:29 +0530

    Update pythonscipts.py

--- pythonscipts.py
+++ pythonscipts.py
@@ -8,3 +8,34 @@ while index < len(x) :
     a.append(b)
     index += 1
 print(a)
+
+
+
+========================================================================
+
+import numpy as np
+
+A = np.array([[1, 4, 5, 12], [-5, 8, 9, 0], [-6, 7, 11, 19]])
+a = []
+i =0
+p = []
+
+for x in A :
+    print(x)
+    print(len(x))
+    b =0
+    for i in range(len(x)) :
+        b = x[i] + b
+        a.append(b)
+        i += 1
+#print(a)
+while a != []:
+    p.append(a[:4])
+    a = a[4:]
+print(p)
+
+q = np.array(p)
+print(q.reshape(3,4))
+
+
+===============
